e80fa96ffb14e44f9b05095f73a12ff5b63fd60a
pmsaru <89391861+pmsaru@users.noreply.github.com>
2021-08-24 20:17:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,22 @@ def load_image(img):
 # Uploading the File to the Page
 selected_box = st.sidebar.selectbox(
     'Choose one of the following',
-    ('Upload Image','Predict')
+    ('About MSI-MSS','Upload Image','Predict')
     )
-
+if selected_box == 'About MSI-MSS':
+    st.write("Microsatellite instability (MSI) is the condition of genetic hypermutability 
+             (predisposition to mutation) that results from impaired DNA mismatch repair (MMR). 
+             The presence of MSI represents phenotypic evidence that MMR is not functioning normally.
+             MMR corrects errors that spontaneously occur during DNA replication, such as single base mismatches or short insertions and deletions. 
+             The proteins involved in MMR correct polymerase errors by forming a complex that binds to the mismatched section of DNA, excises the error, 
+             and inserts the correct sequence in its place.[1] Cells with abnormally functioning MMR are unable to correct errors that occur during 
+             DNA replication and consequently accumulate errors. This causes the creation of novel microsatellite fragments. 
+             Polymerase chain reaction-based assays can reveal these novel microsatellites and provide evidence for the presence of MSI.
+             Microsatellites are repeated sequences of DNA. These sequences can be made of repeating units of one to six base pairs in length. 
+             Although the length of these microsatellites is highly variable from person to person and contributes to the individual 
+             DNA "fingerprint", each individual has microsatellites of a set length. 
+             The most common microsatellite in humans is a dinucleotide repeat of the nucleotides C and A, 
+             which occurs tens of thousands of times across the genome. Microsatellites are also known as simple sequence repeats (SSRs).") 
 if selected_box == 'Upload Image':
     uploadFile = st.file_uploader(label="Upload image")
         
